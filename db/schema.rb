15bb86e935dba4ae74bc6632e99bfd6a18d6db9c
thilo <lathilo@yahoo.de>
2017-03-09 19:32:40 +0100

    4.2 Products added and Style adjusted

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170308144212) do
+ActiveRecord::Schema.define(version: 20170309161630) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20170308144212) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "colour"
+    t.integer  "price"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(:name => "Product 4", :description => "New", :image_url => "Product4.jpg", :colour => "blue", :price => 50)
