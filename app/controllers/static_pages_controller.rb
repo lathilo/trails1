f521f639e5b05985dcda1fcf2ca97a8b76273e89
thilo <lathilo@yahoo.de>
2017-03-23 08:29:29 +0100

    5.3 Contact form added and generating html emails is working

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,6 +5,13 @@ class StaticPagesController < ApplicationController
   def landing_page
     @products = Product.limit(3)
   end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    UserMailer.contact_form(@email, @name, @message).deliver_now
+  end
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/landing_page'
 
+  post 'static_pages/thank_you'
+
   # commented out above line and put in below line cf 3.8 and put back in 5.1
   root 'static_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
