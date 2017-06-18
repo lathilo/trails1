57b3f2cb5dc9ae8177bba245ea2c2ad104a0b45f
thilo <lathilo@yahoo.de>
2017-06-18 14:38:32 +0200

    6.10 Ajax

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,7 +1,7 @@
 /**
  * Created by thilo on 22.04.17.
  */
-$(document).on('turbolinks:load', function(){
+var refreshRating = function() {
     $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
     $('.rated').raty({ path: '/assets',
         readOnly: true,
@@ -9,11 +9,17 @@ $(document).on('turbolinks:load', function(){
             return $(this).attr('data-score');
         }
     });
+};
 
+$(document).on('turbolinks:load ajaxSuccess',function(){
+
+    $('.alert').delay(2000).fadeOut(1500);
+
+    refreshRating();
 
     $('.image-zoom').elevateZoom({
         zoomType: 'lens',
         lensShape: 'round',
         lensSize: 150
-        });
+    });
 });
